a392dce7178c863f6891bb0e5ac17dd6198d3e45
J P C <homeserverstatus@gmail.com>
2017-01-14 11:51:07 +0000

    logging

--- codebase/main_application.py
+++ codebase/main_application.py
@@ -1,14 +1,22 @@
 from common_components.fileprocessing_framework import fileprocessing_module as File
+from . import logging_methods as Log
 
 
 def runapplication(sourcerootpath, destinationrootpath):
 
 	print "==========================================="
 
-	processsubfolder(sourcerootpath, destinationrootpath, "", 0)
+	initiallogs = Log.ApplicationLogs()
+	finallogs = processsubfolder("!ROOT!", sourcerootpath, destinationrootpath, "", initiallogs)
+	finallogs.writelogs(destinationrootpath)
 
 
-def processsubfolder(sourcerootpath, destinationrootpath, currentsubfolder, sublevel):
+
+
+
+def processsubfolder(currentfoldername, sourcerootpath, destinationrootpath, currentsubfolder, logsstart):
+
+	currentlogs = logsstart
 
 	sourcefolderpath = File.concatenatepaths(sourcerootpath, currentsubfolder)
 	targetfolderpath = File.concatenatepaths(destinationrootpath, currentsubfolder)
@@ -16,6 +24,9 @@ def processsubfolder(sourcerootpath, destinationrootpath, currentsubfolder, subl
 	directorylisting = File.getfolderlisting(sourcefolderpath)
 	itemnamelist = directorylisting.keys()
 	itemnamelist.sort()
+	filecount = 0
+	foldercount = 0
+	errorcount = 0
 
 	# Process subfolders and files
 	for itemname in itemnamelist:
@@ -23,21 +34,29 @@ def processsubfolder(sourcerootpath, destinationrootpath, currentsubfolder, subl
 		if (directorylisting[itemname] == "Folder") and (itemname != "@eaDir"):
 
 			newsubfolderpath = File.concatenatepaths(targetfolderpath, itemname)
-			File.makefolder(newsubfolderpath)
+			if File.makefolder(newsubfolderpath) == True:
+				foldercount = foldercount + 1
+			else:
+				currentlogs.logerror("Cannot Make Folder - " + newsubfolderpath)
+				errorcount = errorcount + 1
 
 			newsubfolder = File.concatenatepaths(currentsubfolder, itemname)
-			indenter = " " * sublevel
-			print "     ", indenter, itemname
-			processsubfolder(sourcerootpath, destinationrootpath, newsubfolder, sublevel + 1)
+			print newsubfolder
+			currentlogs = processsubfolder(itemname, sourcerootpath, destinationrootpath, newsubfolder, currentlogs)
 
 		elif directorylisting[itemname] == "File":
 
 			sourcefile = File.concatenatepaths(sourcefolderpath, itemname)
 			targetfile = File.concatenatepaths(targetfolderpath, itemname)
-			File.copyfile(sourcefile, targetfile)
+			if File.copyfile(sourcefile, targetfile) == True:
+				filecount = filecount + 1
+			else:
+				currentlogs.logerror("Cannot Copy File - " + targetfile)
+				errorcount = errorcount + 1
 
 		else:
 			assert directorylisting[itemname] == "Folder", "Unknown File System Object Type"
 
+	currentlogs.logcompletedfolder(currentfoldername, currentsubfolder, filecount, foldercount, errorcount)
 
-	return
+	return currentlogs
